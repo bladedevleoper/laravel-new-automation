c2d59d269bfcfb22768295fc286889f2cf59a6a5
James Jones <37089123+bladedevleoper@users.noreply.github.com>
2021-03-14 21:26:20 +0000

    Add files via upload

---
+++ laravel-new-project.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "What is the project name?"
+read project
+
+if [[ $project != "" ]]
+then curl -s "https://laravel.build/$project" | bash
+else echo "You need to provide a project name, please try again" 
+fi
