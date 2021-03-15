89c89f50688a62b4010c21dc27d7f61069f79997
James Jones <37089123+bladedevleoper@users.noreply.github.com>
2021-03-15 14:13:41 +0000

    tidied the code up

--- laravel-new-project.sh
+++ laravel-new-project.sh
@@ -3,7 +3,8 @@
 echo "What is the project name?"
 read project
 
-if [[ $project != "" ]]
-then curl -s "https://laravel.build/$project" | bash
-else echo "You need to provide a project name, please try again" 
+if [[ $project != "" ]]; then 
+  curl -s "https://laravel.build/$project" | bash
+else 
+  echo "You need to provide a project name, please try again" 
 fi
